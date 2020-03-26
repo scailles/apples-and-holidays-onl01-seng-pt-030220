97020c162ada4ff13c2197d198badaf0cd56398b
Shaina C <scailles23@gmail.com>
2020-03-26 23:04:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.include?("BBQ")
-    push.
+    bbq.push(holiday)
+  else 
+    nil
 end
 
 
