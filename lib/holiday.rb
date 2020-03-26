3efd6a656cc666a95b17231a8e4e7d1735f683cc
Shaina C <scailles23@gmail.com>
2020-03-26 22:53:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.each do |season, holiday|
-  holiday.each do |
+  holiday.each do |holiday, supply|
+    
 
 
 end
