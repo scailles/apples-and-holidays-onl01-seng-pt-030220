469cd1d95a2bb8d25ef136775700ae482851e54d
Shaina C <scailles23@gmail.com>
2020-03-26 22:25:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,9 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   
 holiday_hash.each do |season, holiday|
   season.each do |holiday, supplies|
-    print holiday
-    print supplies
-  end
+    holiday.flatten
+    supplies.flatten
 end
 
 end
